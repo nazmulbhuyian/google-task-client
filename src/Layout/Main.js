9ce32d423ee9c4b33abe29d50b8fa9c072337e90
unknown <nazmulnobel000@gmail.com>
2022-12-29 03:31:06 +0600

    darkmood set

--- src/Layout/Main.js
+++ src/Layout/Main.js
@@ -4,7 +4,7 @@ import Navbar from '../Navbar/Navbar';
 
 const Main = () => {
     return (
-        <div className='dark:bg-gray-900 dark:text-white h-full'>
+        <div className='dark:bg-gray-900 dark:text-white min-h-screen'>
             <Navbar></Navbar>
             <Outlet></Outlet>
         </div>
